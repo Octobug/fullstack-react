f46a1a89f1d80a22acd9de6a01e0d4b2a51aef16
octobug <362499939@qq.com>
2019-02-19 23:41:43 +0800

    static app

--- time-tracking-app/public/js/helpers.js
+++ time-tracking-app/public/js/helpers.js
@@ -1,17 +1,17 @@
-window.helpers = (function () {
+window.helpers = (function() {
   function newTimer(attrs = {}) {
     const timer = {
-      title: attrs.title || 'Timer',
-      project: attrs.project || 'Project',
+      title: attrs.title || "Timer",
+      project: attrs.project || "Project",
       id: uuid.v4(), // eslint-disable-line no-undef
-      elapsed: 0,
+      elapsed: 0
     };
 
     return timer;
   }
 
   function findById(array, id, cb) {
-    array.forEach((el) => {
+    array.forEach(el => {
       if (el.id === id) {
         cb(el);
         return;
@@ -35,8 +35,8 @@ window.helpers = (function () {
     const humanized = [
       pad(hours.toString(), 2),
       pad(minutes.toString(), 2),
-      pad(seconds.toString(), 2),
-    ].join(':');
+      pad(seconds.toString(), 2)
+    ].join(":");
 
     return humanized;
   }
@@ -51,6 +51,6 @@ window.helpers = (function () {
     millisecondsToHuman,
     newTimer,
     findById,
-    renderElapsedString,
+    renderElapsedString
   };
-}());
+})();
