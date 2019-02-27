0a9b7a8c459f1f118c295414603ed3c7ec71333b
octobug <362499939@qq.com>
2019-02-27 22:54:49 +0800

    stateful component

--- components-cookbook/src/components/Switch/Switch.js
+++ components-cookbook/src/components/Switch/Switch.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // import ActiveSpan from '../ActiveSpan/ActiveSpan';
-// import styles from './Switch.css';
+import styles from "./Switch.css";
 
 const CREDITCARD = "Creditcard";
 const BTC = "Bitcoin";
@@ -12,45 +12,37 @@ class Switch extends React.Component {
   state = {
     payMethod: BTC
   };
-  // getInitialState() {
-  //   return {
-  //     payMethod: BTC
-  //   };
-  // },
 
-  // select: function(choice) {
-  //   const that = this;
-  //   return function(evt) {
-  //     that.setState({
-  //       payMethod: choice
-  //     });
-  //   };
-  // },
+  select = choice => {
+    return evt => {
+      this.setState({
+        payMethod: choice
+      });
+    };
+  };
 
-  // _renderChoice: function(choice) {
-  //   return (
-  //     <ActiveSpan onClick={this.select(choice)}
-  //                 active={this.state.payMethod === choice}
-  //                 activeClass={styles.active}
-  //                 className={styles.choice}
-  //                 label={choice} />
-  //   )
-  // },
+  renderChoice = choice => {
+    const cssClasses = [];
 
-  // render: function() {
-  //   return (
-  //     <div className={styles.switch}>
-  //       <div className={styles.diamond} />
-  //       {this._renderChoice(CREDITCARD)}
-  //       {this._renderChoice(BTC)}
-  //     </div>
-  //   )
-  // }
+    if (this.state.payMethod === choice) {
+      cssClasses.push(styles.active);
+    }
+    return (
+      <div
+        className="choice"
+        onClick={this.select(choice)}
+        className={cssClasses}
+      >
+        {choice}
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className="switch">
-        <div className="choice">Creditcard</div>
-        <div className="choice">Bitcoin</div>
+        {this.renderChoice(CREDITCARD)}
+        {this.renderChoice(BTC)}
         Pay with: {this.state.payMethod}
       </div>
     );
